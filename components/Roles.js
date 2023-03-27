0844f621374542ff5381253ed61e9b53233aac44
Isar <isarfridriksson@gmail.com>
2023-03-27 20:12:00 +0000

    Initial commit

---
+++ components/Roles.js
@@ -0,0 +1,27 @@
+//useSWR allows the use of SWR inside function components
+import useSWR from 'swr';
+
+//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Roles() {
+  //Set up SWR to run the fetcher function when calling "/api/staticdata"
+  //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+  const { data, error } = useSWR('/api/staticdata', fetcher);
+
+  //Handle the error state
+  if (error) return <div>Failed to load</div>;
+  //Handle the loading state
+  if (!data) return <div>Loading...</div>;
+  //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+    //Parse the JSON text to Array
+    let parsedData = JSON.parse(data)
+    //Add each role inside a <p> to an array
+    const roles = []
+    parsedData.forEach(role => {
+        roles.push(
+            <p>{role.emoji} - {role.description}</p>
+        )
+    });
+    return roles;
+}
